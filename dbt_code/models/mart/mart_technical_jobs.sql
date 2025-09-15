c7010628d689793d0dc9918a44fb542f53453d89
Efrem <Efremdaniel27@gmail.com>
2025-09-15 10:00:00 +0200

    continuing with marts

--- dbt_code/models/mart/mart_technical_jobs.sql
+++ dbt_code/models/mart/mart_technical_jobs.sql
@@ -4,10 +4,18 @@ with
 select
     f.vacancies,
     f.relevance,
+    e.EMPLOYER__NAME,
     o.occupation,
     o.occupation_group,
     o.occupation_field,
+    j.HEADLINE,
+    j.DURATION,
+    e.workplace_address__municipality,
+    a.DRIVING_LICENSE_REQUIRED,
     f.application_deadline,
 from fct_job_ads f
 left join dim_occupation o on f.occupation_id = o.occupation_id
+left join dim_employer e on f.employer_id = e.employer_id 
+left join dim_job_details j on f.job_details_id = j.job_details_id
+left join dim_auxilliary_attributes a on f.auxilliary_attributes_id = a.auxilliary_attributes_id
 where o.occupation_field = 'Yrken med teknisk inriktning'
